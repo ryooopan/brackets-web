001d907cf3860703c4f8ca8406127301ec827a9b
Ryo Suzuki <ryooopan@googlemail.com>
2015-01-19 13:45:08 +0900

    integrate with brackets

--- app.js
+++ app.js
@@ -3,33 +3,22 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    brackets = require('brackets'),
+    app = express(),
+    server = http.createServer(app);
 
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.get('/', function(req, res) {
+  res.send('Hello world');
 });
+
+var bracketsOpts = {
+  projectsDir: path.join(__dirname, '..'),
+  supportDir:  path.join(__dirname, '..', '/support')
+};
+brackets(server, bracketsOpts);
+
+server.listen(3000);
+console.log('Brackets is avarialable on http://localhost:3000/brackets/');
