259d2200eb8dcdfb8d8389d2c4f7f617989eb81b
Ryo Suzuki <ryooopan@googlemail.com>
2015-01-22 23:30:45 +0900

    public static

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var express = require('express')
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, '.')));
 app.get('/', function(req, res) {
   res.send('Hello world');
 });
